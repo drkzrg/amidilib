be221463212ee293c80901524ea0469713f53cf5
nokturnal <nokturnal>
2012-08-05 08:43:38 +0000

    added missing file from previous commit
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@270 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

---
+++ src/tos/midiReplay.s
@@ -0,0 +1,46 @@
+
+;    Copyright 2007-2012 Pawel Goralski
+;    e-mail: pawel.goralski@nokturnal.pl
+;    This file is part of AMIDILIB.
+;    See license.txt for licensing information.
+
+;    midi delta handling and timing demo
+
+    include "timing/mfp_m68k.inc"
+
+    xdef _midiOutEnabled     ;midi out enabled flag
+    xdef _ymOutEnabled	;ym2149 output enable flag
+    xdef _midiSeqReplay		;custom sequence replay handler
+    
+    xref _tbMode	;data feed to timer, which control
+    xref _tbData	;individual tick duration
+    
+    xref _updateSequenceStep	;our sequence update routine
+    xref update			;our installed interrupt routine
+	even
+_midiSeqReplay:
+	movem.l   d0-7/a0-6,-(a7)	;save registers
+	
+	clr.b     $fffffa1b
+	eor.w	  #$0f0,$ffff8240	;change 1st color in palette (TODO: remove it in the final version)
+	
+	jsr 	_updateSequenceStep
+
+	;prepare next tick
+        move.l    update,$120		;slap interrupt 
+	move.b    _tbData,$fffffa21	;set data
+	move.b    _tbMode,$fffffa1b	;div mode
+	bset.b    #0,$fffffa07		;go!
+	bset.b    #0,$fffffa13
+.finish:	
+	movem.l   (a7)+,d0-7/a0-6	;restore registers
+	bclr.b	  #0,$fffffa0f  	; finished!
+	rte                 		; return from timer
+
+	bss
+
+	even
+_midiOutEnabled:	ds.l	1
+_ymOutEnabled: 	ds.l	1
+	align 4
+

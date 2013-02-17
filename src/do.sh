918560a3f39a1d3131f05fbf2eb13126920d9c46
nokturnal <nokturnal>
2013-02-17 17:15:59 +0000

    launching hatari after build
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@281 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

--- src/do.sh
+++ src/do.sh
@@ -9,8 +9,8 @@ m68k-atari-mint-flags -S ../bin/ym2149.tos
 m68k-atari-mint-flags -S ../bin/midiseq.tos
 m68k-atari-mint-flags -S ../bin/timings.tos
 
-cp -v ../bin/*.tos /home/saulot/Pulpit/HD/0AMIDILIB
-cp -v ../bin/*.tos /home/saulot/Pulpit/shared/amidilib
-cp -v ../bin/*.ttp /home/saulot/Pulpit/HD/0AMIDILIB
-cp -v ../bin/*.ttp /home/saulot/Pulpit/shared/amidilib
-
+cp -v ../bin/*.tos /home/saulot/Pulpit/HD/AMIDI_DEV
+#cp -v ../bin/*.tos /home/saulot/Pulpit/shared/amidilib
+cp -v ../bin/*.ttp /home/saulot/Pulpit/HD/AMIDI_DEV
+#cp -v ../bin/*.ttp /home/saulot/Pulpit/shared/amidilib
+hatari --monitor vga --memsize 14 --bpp 8 --drive-led y --confirm-quit no --midi-in /dev/midi2 --midi-out /dev/midi2 --conout 2  -d /home/saulot/Pulpit/HD/ 

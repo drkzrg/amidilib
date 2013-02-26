e9ab6a351e2ab3cea62dc1d9d88eea668b451a9e
nokturnal <nokturnal>
2013-02-26 16:21:11 +0000

    Added cross tools path prefix optional parameter for build system. Useful under Cygwin,
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@286 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

--- src/do.sh
+++ src/do.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 #clean all stuff
-scons -c
+tools_prefix='/opt/cross-mint'
+
+scons cross=y target='atarist' debug=0 prefix=$tools_prefix ikbd_direct=yes -c
+
 rm ../bin/midiplay.ttp
 rm ../bin/ym2149.tos
 rm ../bin/midiseq.tos
 rm ../bin/timings.tos
 
 #build   
-scons cross=y target=atarist debug=0 ikbd_direct=yes --tree=prune
-m68k-atari-mint-stack ../bin/*.tos --size=128k
-m68k-atari-mint-stack ../bin/*.ttp --size=128k
-m68k-atari-mint-flags -S ../bin/*.tos
+scons cross=y target='atarist' debug=0 prefix=$tools_prefix ikbd_direct=yes --tree=prune
+ 
+$tools_prefix + '/bin/'+ 'm68k-atari-mint-stack' ../bin/*.tos --size=128k
+$tools_prefix + '/bin/'+ 'm68k-atari-mint-stack' ../bin/*.ttp --size=128k
+$tools_prefix + '/bin/'+ 'm68k-atari-mint-flags' -S ../bin/*.tos
 
 #set proper flags
-m68k-atari-mint-flags -S ../bin/midiplay.ttp
-m68k-atari-mint-flags -S ../bin/ym2149.tos
-m68k-atari-mint-flags -S ../bin/midiseq.tos
-m68k-atari-mint-flags -S ../bin/timings.tos
+$tools_prefix + '/bin/'+ 'm68k-atari-mint-flags' -S ../bin/midiplay.ttp
+$tools_prefix + '/bin/'+ 'm68k-atari-mint-flags' -S ../bin/ym2149.tos
+$tools_prefix + '/bin/'+ 'm68k-atari-mint-flags' -S ../bin/midiseq.tos
+$tools_prefix + '/bin/'+ 'm68k-atari-mint-flags' -S ../bin/timings.tos
 
 #adjust stack
 

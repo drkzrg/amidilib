78ae6be34ec11afeb95f1659b77e4a64de244be9
nokturnal <nokturnal>
2013-05-04 11:21:29 +0000

    bugfixes in configuration parsing, better error checking.
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@313 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

--- src/common/core/config.c
+++ src/common/core/config.c
@@ -16,7 +16,7 @@
 //internal configuration
 static tAmidiConfig configuration;
 
-static const U16 CONFIG_VERSION = 0x0001+1+1; 	//config version, changed default event memory pools for events
+static const U16 CONFIG_VERSION = 3; 	//config version, changed default event memory pools for events
 
 static const U8 TRUE_TAG[] = "true";
 static const U8 FALSE_TAG[] = "false";
@@ -39,10 +39,25 @@ static const U8 streamedTag[]={"streamingEnabled"};
 static const U8 lzoCompressionTag[]={"lzoDecompressionEnabled"};
 static const U8 silenceThresholdTag[]={"silenceThreshold"};
 
+//default values
 static const U32 DEFAULT_EVENT_POOL_SIZE =  6000UL; //nb of events
-static const U32 DEFAULT_EVENT_ALLOC_SIZE = 32UL; //event size in bytes
+static const U32 DEFAULT_EVENT_ALLOC_SIZE = 32UL;   //event size in bytes
+static const S32 DEFAULT_MIDI_BUFFER_SIZE = MIDI_BUFFER_SIZE; 	    	    //default operation mode (not used yet)
 
-static const U16 CONFIG_SIZE = 512;		//should be sufficient		 
+static const U16 DEFAULT_CONNECTED_DEVICE_TYPE = DT_LA_SOUND_SOURCE_EXT; 	    //default connected device
+static const U16 DEFAULT_MIDI_CHANNEL = 1; 	    //default midi channel
+static const U16 DEFAULT_OP_MODE = 0; 	    	    //default operation mode (not used yet)
+static const U16 DEFAULT_PLAY_MODE = S_PLAY_ONCE;
+static const U16 DEFAULT_PLAY_STATE = PS_STOPPED;
+static const U16 DEFAULT_MIDI_CONNECTION_TIMEOUT = 5; 	    //external midi module connection timeout
+static const U16 DEFAULT_MIDI_SILENCE_THRESHOLD = 20;    //track silence threshold
+static const BOOL DEFAULT_HANDSHAKEMODE_ENABLED = FALSE;
+static const BOOL DEFAULT_USE_STREAMING = FALSE;
+static const BOOL DEFAULT_USE_LZO = FALSE;
+
+static const U16 CONFIG_SIZE = 512;		    //should be sufficient		 
+
+
 
 S32 parseConfig (const U8* pData, const tMEMSIZE bufferLenght);
 
@@ -114,23 +129,23 @@ void setConnectedDeviceType(eMidiDeviceType type){
 void setDefaultConfig(){
   configuration.version=CONFIG_VERSION;		
   configuration.configSize=sizeof(tAmidiConfig);	
-  configuration.connectedDeviceType = DT_LA_SOUND_SOURCE_EXT; 	//default is CM32L output device with extra patches	
-  configuration.operationMode=0;	
-  configuration.midiChannel = 1;	
-  configuration.playMode = S_PLAY_ONCE;				//play once or in loop
-  configuration.playState = PS_STOPPED;				//default play state: STOPPED or playing
+  configuration.connectedDeviceType = DEFAULT_CONNECTED_DEVICE_TYPE; 	//default is CM32L output device with extra patches	
+  configuration.operationMode=DEFAULT_OP_MODE;	
+  configuration.midiChannel = DEFAULT_MIDI_CHANNEL;	
+  configuration.playMode = DEFAULT_PLAY_MODE;				//play once or in loop
+  configuration.playState = DEFAULT_PLAY_STATE;				//default play state: STOPPED or playing
   configuration.eventPoolSize=DEFAULT_EVENT_POOL_SIZE;
   configuration.eventDataAllocatorSize=DEFAULT_EVENT_ALLOC_SIZE;
   
   #ifndef PORTABLE
-  configuration.midiBufferSize=MIDI_BUFFER_SIZE; //it's atari specific
+  configuration.midiBufferSize=DEFAULT_MIDI_BUFFER_SIZE; //it's atari specific
   #endif
   
-  configuration.midiConnectionTimeOut=5;	//5s by default
-  configuration.midiSilenceThreshold=20;	
-  configuration.handshakeModeEnabled=FALSE;
-  configuration.streamed=FALSE;	
-  configuration.useLZO=FALSE;		
+  configuration.midiConnectionTimeOut=DEFAULT_MIDI_CONNECTION_TIMEOUT;	//5s by default
+  configuration.midiSilenceThreshold=DEFAULT_MIDI_SILENCE_THRESHOLD;	
+  configuration.handshakeModeEnabled=DEFAULT_HANDSHAKEMODE_ENABLED;
+  configuration.streamed=DEFAULT_USE_STREAMING;	
+  configuration.useLZO=DEFAULT_USE_LZO;		
 }
 
 //copies config
@@ -169,30 +184,85 @@ S32 parseConfig(const U8* pData, U32 bufferLenght){
   iError=getUShortVal(versionTag,pData,bufferLenght,&configuration.version); 
    
    // check version if ok then proceed if not throw error
-   if(iError>0 && configuration.version!=CONFIG_VERSION){
-       iError=-1;
-   }
+   if((iError>=0 && configuration.version!=CONFIG_VERSION)){
+     printf("Wrong configuration version. Reseting to defaults.\n");  
+     iError=-1;
+     return (iError);
+  }
   
   
   iError=getUShortVal(connectedDeviceTag,pData,bufferLenght,&configuration.connectedDeviceType);
- 
+  if(iError<0){
+    configuration.connectedDeviceType=DEFAULT_CONNECTED_DEVICE_TYPE;
+  }
+  
   iError=getUShortVal(operationModeTag,pData,bufferLenght, &configuration.operationMode);
+  if(iError<0){
+    configuration.operationMode=DEFAULT_OP_MODE;
+  }
+  
   iError=getUShortVal(midiChannelTag,pData,bufferLenght,&configuration.midiChannel);
+  if(iError<0){
+    configuration.midiChannel=DEFAULT_MIDI_CHANNEL;
+  }
+  
   iError=getUShortVal(playModeTag,pData,bufferLenght,&configuration.playMode);
+  if(iError<0){
+    configuration.playMode=DEFAULT_PLAY_MODE;
+  }
+  
   
   iError=getUShortVal(playStateTag,pData,bufferLenght,&configuration.playState);
+  
+  if(iError<0){
+    configuration.playState=DEFAULT_PLAY_STATE;
+  }
+  
   iError=getUIntVal(eventPoolSizeTag,pData,bufferLenght,&configuration.eventPoolSize);
+  
+  if(iError<0){
+    configuration.eventPoolSize=DEFAULT_EVENT_POOL_SIZE;
+  }
+  
   iError=getUIntVal(eventDataAllocatorSizeTag,pData,bufferLenght,&configuration.eventDataAllocatorSize);
+  if(iError<0){
+    configuration.eventDataAllocatorSize=DEFAULT_EVENT_ALLOC_SIZE;
+  }
+  
   iError=getIntVal(midiConnectionTimeoutTag,pData,bufferLenght,&configuration.midiConnectionTimeOut);
+  
+  if(iError<0){
+    configuration.midiConnectionTimeOut=DEFAULT_MIDI_CONNECTION_TIMEOUT;
+  }
+  
   iError=getIntVal(silenceThresholdTag,pData,bufferLenght,&configuration.midiSilenceThreshold);
+  
+  if(iError<0){
+     configuration.midiSilenceThreshold=DEFAULT_MIDI_SILENCE_THRESHOLD;
+  }
   
   #ifndef PORTABLE
      iError=getIntVal(midiBufferSizeTag,pData,bufferLenght,&configuration.midiBufferSize);
+      if(iError<0){
+	configuration.midiBufferSize=DEFAULT_MIDI_BUFFER_SIZE;
+      }
   #endif
   
   iError=getBoolVal(handshakeCommunicationEnabledTag,pData,bufferLenght,&configuration.handshakeModeEnabled); 
+  if(iError<0){
+    configuration.handshakeModeEnabled=DEFAULT_HANDSHAKEMODE_ENABLED;
+  }
+  
   iError=getBoolVal(streamedTag,pData,bufferLenght,&configuration.streamed);
+  
+  if(iError<0){
+    configuration.streamed=DEFAULT_USE_STREAMING;
+  }
+  
   iError=getBoolVal(lzoCompressionTag,pData,bufferLenght,&configuration.useLZO); 
+  if(iError<0){
+    configuration.useLZO=DEFAULT_USE_LZO;
+  }
   
   return iError;
 }

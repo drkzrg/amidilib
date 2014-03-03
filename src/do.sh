ecfb1ce6c35c79971ce15de39a4a8dba47149f0f
nokturnal <nokturnal>
2014-03-03 23:24:36 +0000

    remote native machine deploy script added, renamed output demo files
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@327 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

--- src/do.sh
+++ src/do.sh
@@ -6,29 +6,40 @@
 #    This file is part of AMIDILIB.
 #    See license.txt for licensing information.
 
-tools_prefix='/usr/'
+tools_prefix='/usr'
+
+# hatari
 install_dir='/home/saulot/Pulpit/HD/AMIDIDEV/'
 emu_parameters='--monitor vga --memsize 14 --bpp 8 --drive-led y --confirm-quit no --midi-in /dev/midi2 --midi-out /dev/midi2 --conout 2'
 emu_dir='/home/saulot/Pulpit/HD/'
 stack_size=128k
 
-#delete binaries if they exist
+# remote machine settings
+REMOTE_MACHINE='192.168.0.5'
+REMOTE_PATH='/c/amidilib/'
+
+MIDIREP_BIN='midiplay.ttp'
+YM2149_TEST_BIN='ym2149.tos'
+MIDIOUT_BIN='midiout.tos'
+MIDISEQ_BIN='midiseq.tos'
+
+# delete binaries if they exist
 echo "############################# Cleaning build .. "
-if [ -f ../bin/midiplay.ttp ]; 
+if [ -f ../bin/$MIDIREP_BIN ];
 then 
-   rm ../bin/midiplay.ttp
+   rm ../bin/$MIDIREP_BIN
 fi
-if [ -f ../bin/ym2149.tos ]; 
+if [ -f ../bin/$YM2149_TEST_BIN ];
 then 
-   rm ../bin/ym2149.tos
+   rm ../bin/YM2149_TEST_BIN
 fi
-if [ -f ../bin/midiseq.tos ]; 
+if [ -f ../bin/$MIDIOUT_BIN ];
 then 
-   rm ../bin/midiseq.tos
+   rm ../bin/$MIDIOUT_BIN
 fi
-if [ -f ../bin/timings.tos ]; 
+if [ -f ../bin/$MIDISEQ_BIN ];
 then 
-   rm ../bin/timings.tos
+   rm ../bin/$MIDISEQ_BIN
 fi
 
 build_options="cross=y target=f030 debug=1 prefix=$tools_prefix ikbd_direct=yes"
@@ -49,31 +60,38 @@ fi
 #copy binaries to install dir
 
 echo "############################# Installation .. "
-if [ -f ../bin/midiplay.ttp ]; 
+if [ -f ../bin/$MIDIREP_BIN ];
 then 
-   $tools_prefix"m68k-atari-mint-stack" ../bin/midiplay.ttp --size=$stack_size
-   $tools_prefix"m68k-atari-mint-flags" -S ../bin/midiplay.ttp
-   cp -v ../bin/midiplay.ttp $install_dir
+   $tools_prefix"m68k-atari-mint-stack" "../bin/"$MIDIREP_BIN --size=$stack_size
+   $tools_prefix"m68k-atari-mint-flags" -S ../bin/$MIDIREP_BIN
+   curl -H "Expect:" --request POST --data-binary "@../bin/$MIDIREP_BIN" $REMOTE_MACHINE$REMOTE_PATH$MIDIREP_BIN
+   cp -v ../bin/$MIDIREP_BIN $install_dir
 fi
 
-if [ -f ../bin/ym2149.tos ];
+if [ -f ../bin/$YM2149_TEST_BIN ];
 then 
-   $tools_prefix"m68k-atari-mint-stack" ../bin/ym2149.tos --size=$stack_size
-   $tools_prefix"m68k-atari-mint-flags" -S ../bin/ym2149.tos
-   cp -v ../bin/ym2149.tos $install_dir
+   $tools_prefix"m68k-atari-mint-stack" ../bin/$YM2149_TEST_BIN --size=$stack_size
+   $tools_prefix"m68k-atari-mint-flags" -S ../bin/$YM2149_TEST_BIN
+   curl -H "Expect:" --request POST --data-binary "@../bin/$YM2149_TEST_BIN" $REMOTE_MACHINE$REMOTE_PATH$YM2149_TEST_BIN
+   cp -v ../bin/$YM2149_TEST_BIN $install_dir
 fi
-if [ -f ../bin/midiseq.tos ];
+if [ -f ../bin/$MIDIOUT_BIN ];
 then 
-   $tools_prefix"m68k-atari-mint-stack" ../bin/midiseq.tos --size=$stack_size
-   $tools_prefix"m68k-atari-mint-flags" -S ../bin/midiseq.tos
-   cp -v ../bin/midiseq.tos $install_dir
+   $tools_prefix"m68k-atari-mint-stack" ../bin/$MIDIOUT_BIN --size=$stack_size
+   $tools_prefix"m68k-atari-mint-flags" -S ../bin/$MIDIOUT_BIN
+   curl -H "Expect:" --request POST --data-binary "@../bin/$MIDIOUT_BIN" $REMOTE_MACHINE$REMOTE_PATH$MIDIOUT_BIN
+   cp -v ../bin/$MIDIOUT_BIN $install_dir
 fi
-if [ -f ../bin/timings.tos ];
+if [ -f ../bin/$MIDISEQ_BIN ];
 then 
-   $tools_prefix"m68k-atari-mint-stack" ../bin/timings.tos --size=$stack_size
-   $tools_prefix"m68k-atari-mint-flags" -S ../bin/timings.tos
-   cp -v ../bin/timings.tos $install_dir
+   $tools_prefix"m68k-atari-mint-stack" ../bin/$MIDISEQ_BIN --size=$stack_size
+   $tools_prefix"m68k-atari-mint-flags" -S ../bin/$MIDISEQ_BIN
+   curl -H "Expect:" --request POST --data-binary "@../bin/$MIDISEQ_BIN" $REMOTE_MACHINE$REMOTE_PATH$MIDISEQ_BIN
+   cp -v ../bin/$MIDISEQ_BIN $install_dir
 fi
+
+#curl -H "Expect:" -X RUN $REMOTE_MACHINE$REMOTE_PATH$(MIDI_REPLAY_BIN)
+
 
 #copy binaries to shared dir
 #cp -v ../bin/*.tos /home/saulot/Pulpit/shared/amidilib

bc164c2fce54b5a4f8ae95a09a106989fae6a2b4
nokturnal <nokturnal>
2014-04-23 14:23:43 +0000

    nktreplay scripts update
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@447 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

---
+++ minimal/nktreplay/nktreplay.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# build helper script
+
+#    Copyright 2007-2014 Pawel Goralski
+#    e-mail: pawel.goralski@nokturnal.pl
+#    This file is part of AMIDILIB.
+#    See license.txt for licensing information.
+
+#general
+#cygwin
+tools_prefix='/opt/cross-mint'
+base_dir='/home/saulot/'
+
+#linux
+#tools_prefix='/usr'
+#base_dir='/home/saulot/'
+
+build_options="cross=y target=f030 debug=1 prefix=$tools_prefix ikbd_direct=yes"
+
+#clean all stuff
+scons $build_options -c
+
+#launch build
+echo "############################# Starting build ... "
+scons $build_options

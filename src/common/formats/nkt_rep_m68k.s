94d121054bc9418acef3ea68cdf07a0831d7c215
nokturnal <nokturnal>
2014-08-23 12:12:42 +0000

    added Midi hook to be integrated into Timer C system call.
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@490 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

--- src/common/formats/nkt_rep_m68k.s
+++ src/common/formats/nkt_rep_m68k.s
@@ -86,7 +86,7 @@ replayNkt:
 
       bra.s	.send
 
-      endif  ;end 68030 part
+      endif  ;end 68030>= part
 
 .done:
         move.w	#0,_MIDIbytesToSend
@@ -112,6 +112,89 @@ replayNkt:
         move.w    (a7)+,sr             ;restore sr
         movem.l   (a7)+,d0-7/a0-6	;restore registers
         rte
+
+
+_NktMidiUpdateHook:
+        movem.l   d0-7/a0-6,-(a7)	; save registers
+        move.w    sr,-(a7)
+        or.w	#$0700,sr		; disable interrupts
+
+        jsr 	_updateStepNkt          ; update sequence state and send events / copy events to internal send buffer
+
+        if (IKBD_MIDI_SEND_DIRECT==1)
+        echo	"[nkt_rep_m68k.s] IKBD MIDI DATA SEND DIRECT ENABLED"
+
+        move.w	#0,d1
+        move.l 	#_MIDIsendBuffer,a0
+        move.w	_MIDIbytesToSend,d1
+
+        cmpi.w   #0,d1
+        beq.s   .done       ;if 0 bytes do nothing
+
+        ifne (__VASM & m68000)
+        echo "[nkt_rep_m68k.s] Midi send 68000 target variant"
+
+.send:
+      ; slap data to d0
+      move.w	(a0),d0  ; get word
+      clr.w	(a0)+ 	 ; clear it
+
+      move.w	d0,d2		;make copy
+      andi.w	#$FF00,d2
+      lsr.w	#8,d2
+.wait1:
+      btst	#1,$fffffc04.w	;is data register empty?
+      beq.s	.wait1		;no, wait!
+      move.b	d2,$fffffc06.w	;write to MIDI data register
+      subq.w	#1,d1
+      cmpi.w	#0,d1
+      beq.s	.done
+
+      ;not done
+      move.w	d0,d2
+      andi.w	#$00FF,d2
+.wait2:
+      btst	#1,$fffffc04.w	;is data register empty?
+      beq.s	.wait2		;no, wait!
+      move.b	d2,$fffffc06.w	;write to MIDI data register
+
+      subq.w	#1,d1
+      cmpi.w	#0,d1
+      beq.s	.done
+
+      bra.s	.send
+
+      else
+      echo "[nkt_rep_m68k.s] Midi send 68030 target variant"
+
+.send:
+      ; slap data to d0
+      move.b	(a0),d0  ; get byte
+      clr.b	(a0)+ 	 ; clear it
+
+.wait:
+      btst	#1,$fffffc04.w	;is data register empty?
+      beq.s	.wait		;no, wait!
+      move.b	d0,$fffffc06.w	;write to MIDI data register
+
+      subq.w	#1,d1
+      cmpi.w	#0,d1
+      beq.s	.done
+
+      bra.s	.send
+
+      endif  ;end 68030>= part
+
+.done:
+        move.w	#0,_MIDIbytesToSend
+        else
+        echo	"[nkt_rep_m68k.s] IKBD MIDI DATA SEND DIRECT DISABLED"
+        endif
+
+        move.w    (a7)+,sr             ;restore sr
+        movem.l   (a7)+,d0-7/a0-6	;restore registers
+        rts
+
 
 
 ; installs / deinstalls MIDI replay (single/multitrack) on selected Timer Interrupt
@@ -174,7 +257,7 @@ _NktDeinstallReplayRout:
 	bset.b    #5,$fffffa09.w        ; enable TiC
 	bset.b    #5,$fffffa15.w        ; set interrupt mask B
 
-        move.w	(sp)+,sr	;restore Status Register
+        move.w	(sp)+,sr                ; restore Status Register
         movem.l (sp)+,d0-d7/a0-a6
         rts
 

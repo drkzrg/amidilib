025c95f13dc7094f473dea7cdb2f2e92569a0aec
nokturnal <nokturnal>
2017-01-14 11:54:09 +0000

    updated minimal version build project. Added support for latest gcc 6.2, moved build parameters to SConstructs (with debug/release postfixes)
    git-svn-id: http://atariforge.org/svn/amidilib/trunk@586 5b2f37e0-8fbc-4d39-81b1-0c51156b4091

--- minimal/makeMinimalLib.sh
+++ minimal/makeMinimalLib.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 # build helper script
 
-#    Copyright 2007-2014 Pawel Goralski
+#    Copyright 2007-2017 Pawel Goralski
 #    e-mail: pawel.goralski@nokturnal.pl
 #    This file is part of AMIDILIB.
 #    See license.txt for licensing information.
 #    This file is part of BadMood/Amidilib.
 
-#general
-#cygwin
-tools_prefix='/opt/cross-mint'
-base_dir='/home/saulot/'
-
-#linux
-#tools_prefix='/usr'
-#base_dir='/home/saulot/'
-
-build_options="cross=y target=f030 debug=0 prefix=$tools_prefix ikbd_direct=yes lzo=no tx_enable=no conout=yes gemdos_io=yes"
-build_options_debug="cross=y target=f030 debug=1 prefix=$tools_prefix ikbd_direct=yes lzo=no tx_enable=no conout=yes gemdos_io=yes"
-
 #clean all stuff
-scons $build_options -c
+scons --sconstruct=Sconstruct_release -c
 
-#launch build
-echo "############################# Starting build ... "
-scons $build_options
-scons $build_options_debug
+#build
+scons --sconstruct=Sconstruct_release
+
+

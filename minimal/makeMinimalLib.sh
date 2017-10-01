7a01a268666d48a2f28bfa749647e65dd209c30e
Unknown <nokturnal@nokturnal.pl>
2017-10-01 16:04:18 +0200

    Cleanup output files after build

--- minimal/makeMinimalLib.sh
+++ minimal/makeMinimalLib.sh
@@ -8,9 +8,17 @@
 #    This file is part of BadMood/Amidilib.
 
 #clean all stuff
+echo ############ Clean All
 scons --sconstruct=SConstruct_release -c
 
 #build
+echo ############ Build All
 scons --sconstruct=SConstruct_release
 
+#remove all intermediate files
+
+echo ############ Cleanup
+find ./nktlib/ -name "*.o" -type f|xargs rm -f
+
+echo Done
 

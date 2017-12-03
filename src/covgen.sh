d782a12b3e252eaf93171e9a312ec7f1adaff5dc
Paweł Góralski <nokturnal@nokturnal.pl>
2017-12-03 13:38:20 +0100

    Cygwin, coverity scan fix
    Set up environment, so coverity scan tools can be launched from Cygwin/Windows. Exchanged GCC 7.xx version from paths, so we can make easier switch to 7.2.

--- src/covgen.sh
+++ src/covgen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 scons --sconstruct=SConstruct_brownerelf -c
-~/cov-analysis/bin/cov-build --dir cov-int scons --sconstruct=SConstruct_brownerelf
+~/cov-analysis/bin/cov-build --dir cov-int sh -c "scons --sconstruct=SConstruct_brownerelf"
 tar czvf amidilib.tgz cov-int
 

7fb960d354e05aab5b8c14230504d818e8ad8690
Paweł Góralski <nokturnal@nokturnal.pl>
2022-05-17 14:34:13 +0200

    moved scripts to separate folder

--- scripts/getLatestStartup.sh
+++ scripts/getLatestStartup.sh
@@ -9,12 +9,19 @@
 # See license.txt for licensing information.
 #
 
-BUILD_ROOT=./
+BUILD_ROOT=.
 
 if [ -d "${BUILD_ROOT}/deps/atari-gcc-startup" ]; then
+echo Deleting gcc startup folder
 rm -rf ${BUILD_ROOT}/deps/atari-gcc-startup
 fi
 
-echo Update dependencies
-wget https://bitbucket.org/nokturnal/atari-gcc-startup/downloads/atari-gcc-startup_latest.tgz -P ${BUILD_ROOT}
+if [ "${BUILD_ROOT}/atari-gcc-startup_latest.tgz" ]; then
+echo Deleting old gcc startup archive
+rm ${BUILD_ROOT}/atari-gcc-startup_latest.tgz
+fi
+
+echo Updating dependencies
+wget -q https://bitbucket.org/nokturnal/atari-gcc-startup/downloads/atari-gcc-startup_latest.tgz -P ${BUILD_ROOT}/
+echo Unpacking minimal gcc startup
 tar -zxvf ${BUILD_ROOT}/atari-gcc-startup_latest.tgz -C ${BUILD_ROOT}/deps

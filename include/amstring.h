6144dbb999dd47b178032f4536e0d56720e0bbf3
Paweł Góralski <nokturnal@nokturnal.pl>
2022-05-28 16:06:55 +0200

    removed extern from function prototypes

--- include/amstring.h
+++ include/amstring.h
@@ -10,25 +10,25 @@
 
 #include "vartypes.h"
 
-AM_EXTERN int memcmp(const void* ptr1, const void* ptr2, size_t num);
-AM_EXTERN void* memcpy(void* destination, const void* source, size_t num);
-AM_EXTERN void* memmove(void* destination, const void* source, size_t num);
-AM_EXTERN void* memset(void* ptr, int value, size_t num);
-AM_EXTERN char* strncpy(char* destination, const char* source, size_t num);
-AM_EXTERN char* strrchr(const char* str, int character);
-AM_EXTERN size_t strlen(const char* str);
-AM_EXTERN int strcmp(const char* str1, const char *str2);
-AM_EXTERN int strncmp(const char* s, const char* t, size_t n);
-AM_EXTERN void* malloc (size_t size);
-AM_EXTERN void* calloc (size_t num, size_t size);
-AM_EXTERN void free (void* ptr);
-AM_EXTERN void* realloc (void* ptr, size_t size);
+int memcmp(const void* ptr1, const void* ptr2, size_t num);
+void* memcpy(void* destination, const void* source, size_t num);
+void* memmove(void* destination, const void* source, size_t num);
+void* memset(void* ptr, int value, size_t num);
+char* strncpy(char* destination, const char* source, size_t num);
+char* strrchr(const char* str, int character);
+size_t strlen(const char* str);
+int strcmp(const char* str1, const char *str2);
+int strncmp(const char* s, const char* t, size_t n);
+void* malloc (size_t size);
+void* calloc (size_t num, size_t size);
+void free (void* ptr);
+void* realloc (void* ptr, size_t size);
 
-AM_EXTERN int tolower(int ch);
-AM_EXTERN int toupper(int ch);
+int tolower(int ch);
+int toupper(int ch);
 
-AM_EXTERN long strtol(const char* nptr, char** endptr, int base);
-AM_EXTERN unsigned long strtoul(const char* nptr, char** endptr, int base);
+long strtol(const char* nptr, char** endptr, int base);
+unsigned long strtoul(const char* nptr, char** endptr, int base);
 
 
 #endif
